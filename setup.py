83a524244c10347700430398f4f533e49f522338
earth-chris <cbanderson08@gmail.com>
2020-07-09 21:50:55 -0700

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+version = open("cfo/__version__.py").read().strip('"\n')
+
+setup(
+    name="cfo",
+    description="Python wrappers and a command line executable for accessing Forest Observatory data via the Salo API",
+    packages=find_packages(),
+    version=version,
+    python_requires=">=3.0",
+    install_requires=["requests", "retrying"],
+    author="Salo Sciences",
+)
