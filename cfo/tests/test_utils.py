0fcbf95d02c6a43da581434cb81ce2dca371a75f
earth-chris <cbanderson08@gmail.com>
2020-07-13 19:30:05 -0700

    added email/pass tests

--- cfo/tests/test_utils.py
+++ cfo/tests/test_utils.py
@@ -1,6 +1,17 @@
+import os
+import builtins
+import mock
 import requests
 
 from cfo import utils
+
+# get authentication inputs
+try:
+    email = os.environ["CFO_EMAIL"]
+    password = os.environ["CFO_PASS"]
+except KeyError:
+    email = "bad@example.com"
+    password = "badpassword"
 
 
 def test_connection():
@@ -19,3 +30,19 @@ def test_endpoints():
         url = "{host}{endpoint}".format(host=utils.URL, endpoint=utils.ENDPOINTS[entry])
         response = requests.get(url)
         assert response.status_code != 404
+
+
+def test_get_input():
+    """Tests user input"""
+    with mock.patch.object(builtins, "input", lambda _: email):
+        assert utils.get_input(email) == email
+        assert utils.get_input(email) != password
+
+
+def test_get_email_pass():
+    """Tests retreival of email/password combo"""
+    e, p = utils.get_email_pass()
+    assert e == email
+    assert p == password
+    assert e != password
+    assert p != email
