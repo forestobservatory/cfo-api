4c34dad19e0868b3b3c93960be9603a875ed825d
salo <cba@salo.ai>
2020-09-01 22:15:43 -0700

    added tons of search utils

--- cfo/utils.py
+++ cfo/utils.py
@@ -1,8 +1,8 @@
 """Convenience functions for interacting with the CFO catalog in the Salo Sciences REST API"""
-import json
 from functools import wraps
 import re
 import logging
+import json
 import os
 import sys
 from getpass import getpass
@@ -31,6 +31,13 @@ ENDPOINTS = {
     "styles": "/data/styles",
     "pixel_pick": "/data/pixel_pick",
 }
+
+# set the path to package data
+package_path = os.path.realpath(__file__)
+package_dir = os.path.dirname(os.path.dirname(package_path))
+json_path = os.path.join(package_dir, "data", "paths.json")
+with open(json_path, "r+") as f:
+    PATHS = json.loads(f.read())
 
 
 def auth_required():
@@ -108,6 +115,57 @@ def get_email_pass():
     return email, password
 
 
+def construct_asset_id(
+    geography: str = None,
+    category: str = None,
+    metric: str = None,
+    year: int = None,
+    timeOfYear: str = None,
+    resolution: int = None,
+):
+    """
+    Constructs a CFO-format asset id string
+    :param geography: see list from API.list_geographies()
+    :param category: see list from API.list_categories()
+    :param metric: see list from API.list_metrics()
+    :param year: julian year
+    :param timeOfYear: can be a date ('0825') or more general ('Fall')
+    :param resolution: the spatial resolution in meters
+    :return asset_id: a formatted string
+    """
+    # handle None
+    if geography is None:
+        geography = "*"
+    if category is None:
+        category = "*"
+    if metric is None:
+        metric = "*"
+    if timeOfYear is None:
+        timeOfYear = "*"
+
+    # handle numerical cases
+    if year is None:
+        year = "*"
+    else:
+        year = f"{year:04d}"
+
+    if resolution is None:
+        resolution = "*"
+    else:
+        resolution = f"{resolution:05d}m"
+
+    # join everything together
+    asset_id = "-".join([geography, category, metric, year, timeOfYear, resolution])
+
+    # replace common wildcards with the sql format wildcard
+    wildcard = "%"
+    finders = ["*", "?"]
+    for finder in finders:
+        asset_id = asset_id.replace(finder, wildcard)
+
+    return asset_id
+
+
 # set the API class
 class API(object):
     """Utility class for Salo API requests"""
@@ -124,6 +182,52 @@ class API(object):
     def help(self):
         """Returns the docstring for the main class"""
         return self.__doc__
+
+    def list_categories(self):
+        """
+        Lists the different categories of data available
+        :return categories: list
+        """
+        return PATHS["category"]
+
+    def list_metrics(self, category: str = None):
+        """
+        Lists the metrics available by category
+        :param category: the category of data for each metric (see .list_categories())
+        :return metrics: a list if category is specified, a dictionary with each metric if not
+        """
+        if category is None:
+            return PATHS["metric"]
+        else:
+            try:
+                return PATHS["metric"][category]
+            except KeyError:
+                LOGGER.warning(f"Unsupported category: {category}")
+                LOGGER.warning(f"Must be one of {', '.join(self.list_categories())}")
+                return None
+
+    def list_geography_types(self):
+        """
+        Lists the broad geography classes (state, watershed, etc.)
+        :return types: list
+        """
+        return list(PATHS["geography"].keys())
+
+    def list_geographies(self, gtype: str = None):
+        """
+        Lists the specific geographic areas available
+        :param gtype: the type of geography to list (see .list_geography_types())
+        :return geographies: a list of type is specified, a dictionary with each geography if not
+        """
+        if type is None:
+            return PATHS["geography"]
+        else:
+            try:
+                return PATHS["geography"][gtype]
+            except KeyError:
+                LOGGER.warning(f"Unsupported category: {gtype}")
+                LOGGER.warning(f"Must be one of {', '.join(self.list_geography_types())}")
+                return None
 
     def authenticate(self):
         """
@@ -151,12 +255,40 @@ class API(object):
         year: int = None,
         timeOfYear: str = None,
         resolution: int = None,
+        asset_id: str = None,
+        bbox: list = None,
+        description: str = None,
         raw: bool = False,
     ):
         """
-        Queries the CFO API for datasets
+        Queries the CFO API for datasets.
+        :param geography: see list from API.list_geographies()
+        :param category: see list from API.list_categories()
+        :param metric: see list from API.list_metrics()
+        :param year: julian year
+        :param timeOfYear: can be a date ('0825') or more general ('Fall')
+        :param resolution: the spatial resolution in meters
+        :param asset_id: an asset id to search by. this is constructed by the above if not passed
+        :param bbox: returns only results that intersect this box [xmin, ymin, xmax, ymax] (in lat/lon)
+        :param description: a string that may match the dataset description
+        :param raw: specifies whether to return the full response object. default is just json
+        :return response: the api search result
         """
-        response = self._search_request()
+        # construct an asset ID if not explicitly passed
+        if asset_id is None:
+            construct_asset_id(
+                geography=geography,
+                category=category,
+                metric=metric,
+                year=year,
+                timeOfYear=timeOfYear,
+                resolution=resolution,
+            )
+
+        # send the request
+        response = self._search_request(asset_id=asset_id, bbox=bbox, description=description)
+
+        # and return the data
         if raw:
             return response
         else:
